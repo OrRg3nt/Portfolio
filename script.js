bafc15a41ea21959e64dabf3183ee1f910d4f0d0
Benjamin <paquet.benjamin.1@gmail.com>
2025-11-12 10:25:12 +0100

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,9 @@
+// Script léger pour animer la navbar et les sections au scroll
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".section").forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
+  });
+});
